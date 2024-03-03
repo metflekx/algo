15d56665c85970c26c5b2d9eccf36704d3b90f8c
dog <dogfuelx@gmail.com>
2024-03-03 13:35:16 -0500

    BSTMin() and BSTMax()

--- BST/BST.h
+++ BST/BST.h
@@ -9,6 +9,8 @@
 
 #include <stdlib.h> // for malloc
 
+#define NODE_NOT_FOUND -1
+
 typedef struct node {
   int key;
   struct node *left;
@@ -16,8 +18,11 @@ typedef struct node {
   struct node *parent;
 } Node;
 
-Node *insert(int key);
-Node *BSTsuccessor(Node *n);
+Node *insert(int);
+Node *BSTMin(Node*);
+Node *BSTMax(Node*);
+Node *BSTSuccessor(Node*);
+Node *BSTSearch(Node*, int);
 
 /* Creates and returns a node n */
 Node *insert(int key) {
@@ -32,15 +37,42 @@ Node *insert(int key) {
   return n;
 }
 
-/* returns node with smallest key greater than n */
-Node *BSTsuccessor(Node *n) {
+/* Returns a ptr to the minimum element int the tree */
+Node *BSTMin(Node *root) {
+  for (; root->left != NULL; root = root->left)
+    ;
+  return (root == NULL)? (Node *)NODE_NOT_FOUND: root;
+}
+
+/* Returns a ptr to the maximum element int the tree */
+Node *BSTMax(Node *root) {
+  for (; root->left != NULL; root = root->right)
+    ;
+  return (root == NULL)? (Node *)NODE_NOT_FOUND: root;
+}
+
+/* Returns node with smallest key greater than n */
+Node *BSTSuccessor(Node *n) {
   if (n->right != NULL)
     return n->right;
   
   Node *p;
   for (p = n->parent; p != NULL && n == p->right; n = p, p = p->parent)
     ;
-  return (p == NULL)? (Node*)NO_MEMORY: p;
+  return (p == NULL)? (Node*)NODE_NOT_FOUND: p;
+}
+
+/* Returns a ptr to a node with key */
+Node *BSTSearch(Node *root, int key) {
+  if (root == NULL)
+    return (Node*)NODE_NOT_FOUND;
+  if (root->key == key)
+    return root;
+
+  if (key > root->key) 
+    return BSTSearch(root->right, key);
+  else
+    BSTSearch(root->left, key);
 }
 
 #include <stdio.h> // for printing
